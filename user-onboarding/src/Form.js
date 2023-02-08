15fd462c243e8206efcf73973cce00f68d4d5279
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-08 05:22:41 -0500

    added inputs inside form

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -5,6 +5,55 @@ export default function Form(props){
 
 
     return (
-<h1>h3llo</h1>
-    )
+
+        <form>
+            <label for="firstName">
+                First Name
+                <input 
+                name="firstName" 
+                type="text"/>
+            </label>
+
+
+            <label for="lastName">
+                Last Name
+                <input 
+                    type="text"
+                    name="lastName"/>
+                     </label>
+            <label for="email">
+                Email
+                <input 
+                name="email" 
+                type="email"/>
+            </label>
+            <label for="passWord">
+                Password
+                <input 
+                name="passWord"
+                type="text"
+                />
+            </label>
+            <label for="checkBox">
+                Terms Of Service
+                <input name="checkBox" type="checkbox"/>
+            </label>
+
+
+        </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            )
 }       
